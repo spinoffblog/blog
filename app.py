334cdfc921dcd29706efaa6714eedf265d9afd1c
Your Name <174594389+spinoffblog@users.noreply.github.com>
2024-07-03 16:58:41 +0800

    cont’d

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.title('Hello, Streamlit!')
-st.write('This is a test app.')
+st.write('This is a test app - running from a remote python script :)')
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Run the application
+python -m streamlit run app.py
