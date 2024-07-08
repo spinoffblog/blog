3d55f8aefda76f85b0fbf0ec0174b7059cc60685
Your Name <174594389+spinoffblog@users.noreply.github.com>
2024-07-08 15:04:48 +0800

    cont'd

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 # Update the app and restart the Streamlit service to apply the changes
-# Get the directory of the script
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# Navigate to the root directory (assuming it's one level up from scripts)
-cd "$SCRIPT_DIR/.."
 
 git remote update
 git rebase origin/master
 source ../venv/bin/activate
-pip install -r requirements.txt
+pip install -r ../requirements.txt
 sudo systemctl restart streamlit.service
