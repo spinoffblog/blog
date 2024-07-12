8f430a8aae8471b3562208c94bb6e04549470f8f
Your Name <174594389+spinoffblog@users.noreply.github.com>
2024-07-12 13:26:36 +0800

    typo

--- scripts/update.sh
+++ scripts/update.sh
@@ -7,7 +7,7 @@ git rebase origin/master
 source ../venv/bin/activate
 pip install -r ../requirements.txt
 
-cd scripts/og_tag_generator
+cd og_tag_generator
 python og_tag_generator.py
 cp output/* /var/www/spinoff/static_html/
 sudo chmod 755 /var/www/spinoff/ && sudo chown -R www-data:www-data /var/www/spinoff/
