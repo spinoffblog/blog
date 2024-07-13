252dc9b3f13cf8e3d62344c39f4521768278637e
Your Name <174594389+spinoffblog@users.noreply.github.com>
2024-07-13 17:33:17 +0800

    like the comments in the script say

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,6 +2,12 @@
 
 # Update the app and restart the Streamlit service to apply the changes
 
+# if the git repository is not clean, exit
+if [ -n "$(git status --porcelain)" ]; then
+    echo "The git repository is not clean. Please commit your changes before updating."
+    exit 1
+fi
+
 git remote update
 git rebase origin/master
 source ../venv/bin/activate
