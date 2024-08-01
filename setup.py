b6f153c97ae1394cbf7224b72b309d40b937b867
Your Name <174594389+spinoffblog@users.noreply.github.com>
2024-08-01 15:37:29 +0800

    wip

--- setup.py
+++ setup.py
@@ -1,16 +1,34 @@
 from setuptools import setup, find_packages
 
+# Read the contents of your README file
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+# Read requirements from requirements.txt
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
-    name="my_streamlit_app",
+    name="spinoff_blog",
     version="0.1",
+    author="spinoffblog",
+    author_email="your.email@example.com",  # You may want to update this
+    description="A Streamlit app for the Spinoff Blog",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/spinoffblog/blog",
     packages=find_packages(exclude=["tests"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
     entry_points={
         "console_scripts": [
-            "my-streamlit-app=my_streamlit_app.main:run",
+            "spinoff-blog=spinoff_blog.main:run",
         ],
     },
-    install_requires=[
-        "streamlit",
-        # Add other dependencies
-    ],
+    install_requires=requirements,
+    include_package_data=True,
 )
